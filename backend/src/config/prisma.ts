96e1b1c69e30c463dd8414de3aaaca5e0d775fa0
shivean <shivaramm222@gmail.com>
2026-06-11 23:48:19 +0545

    update prisma config

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 // Prisma Client singleton instance
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
