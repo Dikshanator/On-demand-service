ce35ac8755fc6cd381838d6bbb4c65c2f9762963
shivean <shivaramm222@gmail.com>
2026-06-13 22:39:47 +0545

    add email templates

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -1,4 +1,8 @@
 import nodemailer from "nodemailer";
+import {
+  Verification_Email_Template,
+  Welcome_Email_Client,
+} from "../libs/EmailTemplate";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -8,21 +12,20 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendVerificationEmail = async (
-  email: string,
-  code: string
-) => {
+export const sendVerificationEmail = async (email: string, code: string) => {
   await transporter.sendMail({
     from: `"On-Demand Service" <${process.env.EMAIL_USER}>`,
     to: email,
-    subject: "Verify your email",
-    html: `
-      <div style="font-family: Arial;">
-        <h2>Email Verification</h2>
-        <p>Your verification code is:</p>
-        <h1 style="letter-spacing: 4px;">${code}</h1>
-        <p>This code will expire in <b>10 minutes</b>.</p>
-      </div>
-    `,
+    subject: "Verify Your Account",
+    html: Verification_Email_Template.replace("{verificationCode}", code),
+  });
+};
+
+export const sendWelcomeEmailClient = async (email: string, name: string) => {
+  await transporter.sendMail({
+    from: `"On Demand Service" ${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: "Welcome to Our App",
+    html: Welcome_Email_Client.replace("{name}", name),
   });
 };
