fc1518077fa2c274b71f547f6343d09076ff9839
shivean <shivaramm222@gmail.com>
2026-06-13 22:35:00 +0545

    update rate limiter

--- backend/src/middleware/Ratelimiter.ts
+++ backend/src/middleware/Ratelimiter.ts
@@ -1,7 +1,17 @@
 import rateLimit from 'express-rate-limit';
+import { Request } from 'express';
 
 export const authRateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
-    message: 'Too many requests from this IP, please try again after 15 minutes'
+    message: 'Too many requests from this IP, please try again after 15 minutes',
+    standardHeaders: true,
+    legacyHeaders: false,
 })
+
+export const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // Limit auth attempts
+    message: 'Too many login attempts, please try again later',
+    skip: (req: Request) => req.method !== 'POST',
+});
